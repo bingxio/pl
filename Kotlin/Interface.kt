b2281dd96d469cff55aa89ed8050234dc803fd6a
Turaiiao <3106740988@qq.com>
2019-05-15 22:26:02 +0800

    update

---
+++ Kotlin/Interface.kt
@@ -0,0 +1,31 @@
+
+interface UserModel {
+    val prop: Int
+
+    fun login (username: String, password: String): Boolean
+
+    fun show () = println(prop)
+
+    fun create () {}
+}
+
+interface People : UserModel {
+    override val prop: Int get() = 50
+}
+
+class UserModelImp : UserModel, People {
+
+    override val prop: Int get() = 20
+
+    override fun login(username: String, password: String): Boolean {
+        show().let {
+            return !(username.isEmpty() || password.isEmpty())
+        }
+    }
+}
+
+fun main() {
+    val userModelImp = UserModelImp()
+
+    println(userModelImp.login("turaiiao", ""))
+}
