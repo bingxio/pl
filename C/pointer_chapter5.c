6b9cacbf8a8d11e30754866389d151334cde568f
Turaiiao <3106740988@qq.com>
2019-05-16 21:57:35 +0800

    update

---
+++ C/pointer_chapter5.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int min(int a, int b) {
+    return a > b ? b : a;
+}
+
+int main() {
+    int (* a) (int, int) = max;
+    int (* b) (int, int) = min;
+
+    int s = 0, d = 0;
+
+    printf("input two number to print max value and min value: \n");
+
+    scanf("%d %d", &s, &d);
+
+    printf("max = %d, min = %d \n", a(s, d), b(s, d));
+}
