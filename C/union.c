3faec290387f02e8f18dca9df5be3419ec985eed
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-10 18:31:42 +0800

    Create union.c

---
+++ C/union.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+	int precdence;
+	
+	union {
+		bool boolean;
+		double number;
+	} as;
+} Value;
+
+#define NUMBER_VAL(value) ((Value) { 1, { .number = value } })
+
+int main() {
+	Value a = NUMBER_VAL(12.5);
+	
+	printf("precdence = %d \n", a.precdence);
+	printf("it number = %f \n", a.as.number);
+}
