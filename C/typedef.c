d6976ef4c168404c7aabdccd8ed98e24aedcd99e
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-10 13:16:32 +0800

    Create typedef.c

---
+++ C/typedef.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef int (* function) (char* message);
+
+int test(char* message) {
+	return strlen(message);
+}
+
+int main() {
+	function a = test;
+	
+	int len = a("hello world");
+	
+	printf("byte = %d \n", len + 1);
+	
+	return 0;
+}
