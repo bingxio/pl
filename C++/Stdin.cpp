859eef719e3bcc8faeee863902c388c2c95cd61b
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-11 16:08:53 +0800

    Create Stdin.cpp

---
+++ C++/Stdin.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    char* username = (char *) malloc(64);
+    
+    cout << "Please input your name: ";
+    
+    /**
+     * turaiiao
+     */
+    cin >> username;
+    
+    string a = username;
+    
+    /**
+     * username = turaiiao, length = 8
+     */
+    cout << "username = " << username;
+    cout << ", length = " << a.size() << endl;
+    
+    return 0;
+}
