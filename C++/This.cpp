74e16c1ae2a66df007c326e432fe7cca676714f6
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-12 17:29:44 +0800

    Create This.cpp

---
+++ C++/This.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class User {
+    public:
+        User(string name = "turaiiao", string pass = "12345678") {
+            username = name;
+            password = pass;
+        }
+        
+        bool compare(User user);
+    private:
+        string username;
+        string password;
+};
+
+bool User::compare(User user) {
+    return this->username == user.username;
+}
+
+int main() {
+    
+    User user1;
+    User user2("frank", "87654321");
+    
+    if (user1.compare(user2)) {
+        cout << "User name of User 1 is the same as User 2." << endl;
+    } else {
+        cout << "User name of User 1 is different from User 2." << endl;
+    }
+    
+    return 0;
+}
