8da512e4c7ccdebd1d56cc1396eee5a120ece53a
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-13 20:00:16 +0800

    Create lambda.py

---
+++ Python/lambda.py
@@ -0,0 +1,31 @@
+
+# Basic lambda function.
+a = lambda a, b, c: a + b + c
+
+# The lambda function has no input parameters and the output is None.
+b = lambda: None
+
+# Lambda function with uncertain parameters.
+c = lambda *args: max(args)
+
+# And other statements.
+d = lambda a, b: a if a < b else b
+
+if __name__ == '__main__'
+    print('12 + 43 + 1 = %d' % a(12, 43, 1))
+	  print(b())
+	  print('max (12, -23) = %d' % c(12, -23))
+	  print('min (12, -23) = %d' % d(12, -23))
+
+	  list = []
+	
+	  for i in range(0, 20):
+		  list.append(i)
+		
+	  print(list)
+	
+	  a = filter(lambda x: x % 2 == 0, list)
+	  b = map(lambda x: x + 5, a)
+	
+	  print(a)
+	  print(b)
