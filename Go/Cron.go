3f5371dbacf9e43999ff6a6fc18f196df5c0f93a
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-13 22:15:51 +0800

    Update Cron.go

--- Go/Cron.go
+++ Go/Cron.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "time"
-    "fmt"
-    
-    "github.com/robfig/cron"
+	"fmt"
+	"time"
+
+	"github.com/robfig/cron"
 )
 
 func main() {
-    code := "0 0 * * * ?"
-    
-    c := cron.New()
-    
-    c.AddFunc(code, func() {
-        currentTime := time.Now().Format("2006-01-02 15:04")
-        
-        fmt.Println(currentTime)
-    })
-    
-    c.Start()
-    
-    select {}
+	code := "0 0 * * * ?"
+
+	c := cron.New()
+
+	c.AddFunc(code, func() {
+		currentTime := time.Now().Format("2006-01-02 15:04")
+
+		fmt.Println(currentTime)
+	})
+
+	c.Start()
+
+	select {}
 }
