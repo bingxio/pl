f81c8eb1457c305e7843666b281af8bd8d82274f
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-13 22:16:13 +0800

    Update Ticker.go

--- Go/Ticker.go
+++ Go/Ticker.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "time"
-    "fmt"
+	"fmt"
+	"time"
 )
 
 func main() {
-    ticker := time.NewTicker(time.Second * 300)
-    
-    counter := 0
-    
-    go func() {
-        for range ticker.C {
-            fmt.Printf("call ticker for %d count. \n", counter)
-            
-            counter ++
-            
-            if counter == 5 {
-                ticker.Stop()
-            }
-        }
-    }()
+	ticker := time.NewTicker(time.Second * 300)
+
+	counter := 0
+
+	go func() {
+		for range ticker.C {
+			fmt.Printf("call ticker for %d count. \n", counter)
+
+			counter++
+
+			if counter == 5 {
+				ticker.Stop()
+			}
+		}
+	}()
 }
