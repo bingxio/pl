8a891cfedb9db512dbc304791957fa20f62d6dc1
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-14 17:55:24 +0800

    Update New.cpp

--- C++/New.cpp
+++ C++/New.cpp
@@ -3,23 +3,33 @@
 using namespace std;
 
 class User {
-	public:
-		string username;
-		string password;
+    public:
+        string username;
+	string password;
 	
-		User(string name, string pass);
+	User(string name, string pass);
 };
 
 User::User(string name, string pass): username(name), password(pass) {}
 
 int main() {
+
+    /**
+     * Use new keyword to create new reference.
+     *
+     * if there are no parameters, don't write parentheses.
+     *
+     * User* a = new User;
+     */
+    User* a = new User("turaiiao", "12345678");
 	
-	User* a = new User("turaiiao", "12345678");
+    cout << a -> username << endl;
+    cout << a -> password << endl;
+
+    /**
+     * Must use delete keyword to release memory.
+     */
+    delete a;
 	
-	cout << a -> username << endl;
-	cout << a -> password << endl;
-	
-	delete a;
-	
-	return 0;
+    return 0;
 }
