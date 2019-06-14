daf57d004470ca9ffb7131a637f82d8f5f1c4aab
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-06-14 20:05:47 +0800

    Create Explicit.cpp

---
+++ C++/Explicit.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class Male {
+    public:
+    
+        /**
+         * If use explicit keyword, represents that this function is an explicit transformation.
+         */
+        explict Male(int a);
+        
+        ~Male();
+};
+
+Male::Male(int a) {
+    cout << "object is created." << endl;
+}
+
+Male::~Male() {
+    cout << "object is deleted." << endl;
+}
+
+void fun(Male* male) {}
+
+int main() {
+
+    Male* a = new Male("turaiiao");
+
+    /**
+     * If it's not an explicit conversion, this parameter can be passed as an integer.
+     */
+    fun(a);
+    
+    delete a;
+    
+    return 0;
+}
