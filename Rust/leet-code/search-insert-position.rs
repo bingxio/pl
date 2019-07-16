dcdea101f39027a343bbda1977adb8e98a5085de
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-07-16 12:23:02 +0800

    Create search-insert-position.rs

---
+++ Rust/leet-code/search-insert-position.rs
@@ -0,0 +1,33 @@
+impl Solution {
+    
+    // search insert position to array.
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        
+        // default insert is -1, if cannot find insert position,
+        let mut position: i32 = -1;
+        
+        // iterative array to find insert position.
+        for (i, &v) in nums.iter().enumerate() {
+            // if iterator equal target or greater target,
+            // position assign the i, then break.
+            if v == target || v > target {
+                // find out, assign the index to i.
+                position = i as i32;
+                
+                break;
+            } else if v < target {
+                // if target greater than v,
+                // explain that the target value is behind.
+                continue;
+            }
+        }
+        
+        // if no suitable insertion location is found.
+        // set length of assignment array.
+        return if position == -1 {
+            nums.len() as i32
+        } else {
+            position
+        }
+    }
+}
