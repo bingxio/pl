d1f8dc67e15e0d52422364e7e3fa4b4f1a99023f
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-08-20 13:51:00 +0800

    Update union.c

--- C/union.c
+++ C/union.c
@@ -14,6 +14,7 @@ int main() {
     // The union type can only be set to same type as this.
     Value a = { 23, { .isNumber = false, .value = 2.4 } };
     
+    // It is always true, because it address is "next door".
     if (a.as.isNumber) {
         printf("%d %d %f\n", a.precedence, a.as.isNumber, a.as.value);
         // If it is a different type.
