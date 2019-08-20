1f93e644d39d1171c7218d0c2fbddd758eb3d136
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-08-20 13:49:52 +0800

    Update union.c

--- C/union.c
+++ C/union.c
@@ -11,10 +11,15 @@ typedef struct {
 } Value;
 
 int main() {
+    // The union type can only be set to same type as this.
     Value a = { 23, { .isNumber = false, .value = 2.4 } };
-	
+    
     if (a.as.isNumber) {
         printf("%d %d %f\n", a.precedence, a.as.isNumber, a.as.value);
+        // If it is a different type.
+        // Value a;
+        // a.as.isNumber = false;
+        // a.as.value = 1.2;
     }
     
     return 0;
