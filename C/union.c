ae92417cc33eefd191600fd52086841b83c76df1
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-08-20 12:27:43 +0800

    Update union.c

--- C/union.c
+++ C/union.c
@@ -2,24 +2,20 @@
 #include<stdbool.h>
 
 typedef struct {
-    int precedence;
-
-    union {
-	    bool isNumber;
-	    double value;
-    } as;
+	int precedence;
+	
+	union {
+		bool isNumber;
+		double value;
+	}
 } Value;
 
 int main() {
-    Value a = { 23, {
-		    .isNumber = false,
-		    .value = 2.4
-	    },
-    };
-
-    if (a.as.isNumber) {
-	    printf("%d %d %f\n", a.precedence, a.as.isNumber, a.as.value);
-    }
-
-    return 0;
+	Value a = { 23, {
+			.isNumber = false,
+			.value = 2.4,
+		},
+	};
+	
+	return 0;
 }
