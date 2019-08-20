d4dc6c18cdf76f4dbed66c9ce90af4ccf3ca93c2
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-08-20 12:29:00 +0800

    Create union.c

---
+++ C/union.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef struct {
+	int precedence;
+
+	union {
+		bool isNumber;
+		double value;
+	} as;
+} Value;
+
+int main() {
+	Value a = { 23, {
+			.isNumber = false,
+			.value = 2.4
+		},
+	};
+
+	if (a.as.isNumber) {
+		printf("%d %d %f\n", a.precedence, a.as.isNumber, a.as.value);
+	}
+
+	return 0;
+}
