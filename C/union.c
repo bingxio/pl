ceb9f69f4d2230aebfcf6b6b3535121114650d9e
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-08-20 12:28:16 +0800

    Update union.c

--- C/union.c
+++ C/union.c
@@ -3,19 +3,23 @@
 
 typedef struct {
 	int precedence;
-	
+
 	union {
 		bool isNumber;
 		double value;
-	}
+	} as;
 } Value;
 
 int main() {
 	Value a = { 23, {
 			.isNumber = false,
-			.value = 2.4,
+			.value = 2.4
 		},
 	};
-	
+
+	if (a.as.isNumber) {
+		printf("%d %d %f\n", a.precedence, a.as.isNumber, a.as.value);
+	}
+
 	return 0;
 }
