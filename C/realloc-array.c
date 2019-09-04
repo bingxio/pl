b5cc43c1c55554f60ff81cb97da41f68d9b9d166
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-09-04 10:57:16 +0800

    Update realloc-array.c

--- C/realloc-array.c
+++ C/realloc-array.c
@@ -4,24 +4,24 @@
 
 int main() {
   int* code = NULL, size = 0;
+  
+  int a[] = {23, 45, 65, 3, 43, 23, 32, 0}, i = 0;
 
-	int a[] = {23, 45, 65, 3, 43, 23, 32, 0}, i = 0;
-
-	while (true) {
-		if (a[i] > 0) {
-			if (i >= size)
-				code = (int *) realloc(code, size);
-			code[size ++] = a[i ++];
-		} else
-			break;
+  while (true) {
+    if (a[i] > 0) {
+      if (i >= size)
+        code = (int *) realloc(code, size);
+      code[size ++] = a[i ++];
+    } else 
+      break;
   }
 
-	printf("size = %d\n", size);
+  printf("size = %d\n", size);
 
-	for (int i = 0; i < size; i ++)
-		printf("%-5d", code[i]);
-
+  for (int i = 0; i < size; i ++)
+    printf("%-5d", code[i]);
+  
   free(code);
 
-	return 0;
+  return 0;
 }
