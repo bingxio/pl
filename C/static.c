f74a6d7dbe3c79c0c1d863a815810e441c3b15a2
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-10-04 18:50:48 +0800

    Update static.c

--- C/static.c
+++ C/static.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-void static_func () {
+void static_value () {
   static int a = 20;
   
   printf("++ a = %d\n", ++ a);
 }
 
+static void static_func () {
+  printf("Call static function.");
+}
+
 int main() {
+  static_value();
+  static_value();
+  static_value();
+  
   static_func();
-  static_func();
-  static_func();
+  
   return 0;
 }
