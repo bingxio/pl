756485e3cac8c1f7f571932c86926d505987020c
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-11-01 21:12:53 +0800

    Update lambda-dsl-generic.kt

--- Kotlin/lambda-dsl-generic.kt
+++ Kotlin/lambda-dsl-generic.kt
@@ -8,9 +8,20 @@ fun main() {
     println(a)
   }
   Person("turaiiao")()
+  println(
+    12.add(34)
+  )
 }
 
 private fun sum(term: (Int, Int) -> String, x: Int, y: Int): String = term(x, y)
+
+private fun <T> Int.add(x: T): Int {
+  if (x !is Int) {
+    println("x is not a number !")
+    return -1
+  }
+  return x.toInt() + this
+}
 
 private fun verticalLinearLayout(func: () -> Unit) {
   func()
