d463c300f4d4724ad1dbbdbc1910f9d0f0ce35f5
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-11-16 10:46:08 +0800

    Update all-dart-tutorial-en.dart

--- Dart/all-dart-tutorial-en.dart
+++ Dart/all-dart-tutorial-en.dart
@@ -1,30 +1,49 @@
+// The tutorial for Dart programming language write at Nov 15 2019.
+
+// import <path> | as <alias> | show <field...> ;
+// The `dart:` is standard library in Dart.
 import 'dart:convert';
 
+// If the function not have return type I can ignore it.
+// The `main` function is can be asynchronous.
 main() async {
+  // I can use `var` keyword to automatic judgment type of variable.
   var x = 56;
-  
+  // And explain the type of variable.
+  // The only one or double colon fine assign a string literal.
   String str = 'Hello,';
-
+  // Output string with template.
   print('$str Dart !');
 
+  // Assign a empty list.
   var arr = [];
-
+  // First kind to use `for` loop statement.
+  // for (<initExpr>; <CondExpr>; <UpExpr>) {}
   for (var i = 0; i < 20; i ++)
     arr.add(i);
-
+  // 20
+  // Use `${expr}` to get expression string literal.
+  // The `length` method is standrad libray for array list std.
   print('arr len = ${arr.length}');
-
+  // Second kind to use `for` loop statement.
+  // for (var <ident> in <arr>) {}
   for (var i in arr) {
+    // Traversing the value of an array.
     if (i % 2 == 0) {
       print(i);
     }
   }
-  
+  // And use lambda expression.
   arr.forEach((value) {
     print('value = $value');
   });
 
+  // Declare a new object without `new` keyword after 2.5 version.
   var user = User();
+  // This method is to setter age member variable.
+  user.Age = 34;
+  // Get member variable and output.
+  print(user.age);
 
   await login('turaiiao', '12345678').then((data) {
     if (!(data as Map<String, dynamic>).containsValue('message')) {
