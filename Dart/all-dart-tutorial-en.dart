339849a178fd0dfb96a97f0286f7780869a8abb3
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-11-16 11:06:54 +0800

    Update all-dart-tutorial-en.dart

--- Dart/all-dart-tutorial-en.dart
+++ Dart/all-dart-tutorial-en.dart
@@ -41,9 +41,40 @@ main() async {
   // Declare a new object without `new` keyword after 2.5 version.
   var user = User();
   // This method is to setter age member variable.
-  user.Age = 34;
+  user.age = 45;
   // Get member variable and output.
-  print(user.age);
+  print('${user.age} -> ${user.ageString}');
+
+  int add(int a, int b) => a + b;
+  // At this function we used alias parameter.
+  // { <aliasParameters...> }
+  int sub(int a, int b, {String msg, bool reverse}) {
+    print(msg);
+
+    if (reverse) {
+      return b - a;
+    }
+    return a - b;
+  }
+  // Optinal parameter.
+  // [ <optinalParameters...> ]
+  alert(String msg, [int count]) {
+    // If not declare optional parameter it will be null.
+    // Called will throw some exception.
+    if (count != null) {
+      for (var i = 0; i < count; i ++)
+        print(msg);
+    }
+  }
+
+  print(add(45, 66));
+  // Use name of parameter to appoint alias parameter.
+  // We must use alias parameter and unknown can be arbitrary.
+  print(sub(65, 23, reverse: false, msg: 'Testing...'));
+
+  alert('HelloWorld');
+  // The optional parameter must One-to-one correspondence.
+  alert('DART !', 5);
 
   await login('turaiiao', '12345678').then((data) {
     if (!(data as Map<String, dynamic>).containsValue('message')) {
@@ -51,13 +82,20 @@ main() async {
     }
   });
 }
-
+// Declare a new class object.
 class User {
+  // Member variable.
+  // Default to setter method.
   int age;
-
-  set Age(int age) => age = age;
-
-  String get ageString => '${this.age}';
+  // Getter method.
+  String get ageString => 'Age = ${this.age}';
+  // Constructor function.
+  User(/** construction variable can be here. */) {
+    print("Object will be created...");
+  }
+  // Must be one construction method !
+  // User(int age) {
+  // }
 }
 
 Future<dynamic> login(String username, String password) async {
