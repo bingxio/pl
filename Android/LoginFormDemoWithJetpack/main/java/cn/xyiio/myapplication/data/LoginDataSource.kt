6cff92bcdda5ada7243c330d85055191b87cf2ad
Turaiiao <3106740988@qq.com>
2020-02-06 18:56:04 +0800

    Update

---
+++ Android/LoginFormDemoWithJetpack/main/java/cn/xyiio/myapplication/data/LoginDataSource.kt
@@ -0,0 +1,25 @@
+package cn.xyiio.myapplication.data
+
+import cn.xyiio.myapplication.data.model.LoggedInUser
+import java.io.IOException
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        return try {
+            // TODO: handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
+
---
+++ Android/LoginFormDemoWithJetpack/main/java/cn/xyiio/myapplication/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package cn.xyiio.myapplication.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
