fec4533996c38f795ae0ba039b73ff6efe2ad1b3
丙寅乙 <3106740988@qq.com>
2020-04-14 09:44:43 +0800

    Update Switch.java

--- Java/Switch.java
+++ Java/Switch.java
@@ -1,20 +1,15 @@
-package app;
-
-class App {
-  public static void main (String[] args) throws Exception {
-    int a = 20;
-    
-    int b = switch (a) {
-      case 0 -> -1;
-      case 10 -> 12;
-      case 20 -> 30;
-
-      default -> {
-        yield 50;
-        System.out.println("Call default branch ...");
-      }
-    };
-
-    System.out.println("a = " + a + " b = " + b);
-  }
+public class Main {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = switch (x) {
+            case 29 -> -1;
+            case 30 -> {
+                System.out.println("Call.");
+                yield 32;
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + x);
+        };
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
 }
