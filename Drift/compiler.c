b16450261b49325e900c96f3939d6d6f90f3171f
丙杺 <3106740988@qq.com>
2021-07-12 02:24:51 -0400

    Update compiler.c

--- Drift/compiler.c
+++ Drift/compiler.c
@@ -7,38 +7,50 @@
 typedef enum { LITERAL, NUMBER, STR, CHAR } token_kind;
 
 typedef struct { token_kind kind; const char *literal; int line; } token;
-typedef struct { int count; token *tokens[]; } lexer_result;
-
-bool is_space(char c, int i, int fsize) { 
-	return (c == ' ' || c == '\t' || c == '\n' || c == '\r') && i < fsize;
-}
+typedef struct { int len; int cap; token *tokens; } lexer_result;
 
 bool peek_to(char c, char p, int i, int fsize, const char *buf) {
 	return c == '/' && i + 1 != fsize && buf[i + 1] == p;
 }
 
+bool is_space(char c) { return c == ' ' || c == '\t' || c == '\n' || c == '\r'; }
 bool is_digit(char c) { return c >= '0' && c <= '9'; }
-bool is_identifier(char c) {
+bool is_ident(char c) {
 	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_';
 }
 
-token *new_token(token_kind k, const char *literal, int line) {
-	token *t = (token *) malloc(sizeof(token));
-	t->kind = k;
-	t->literal = literal;
-	t->line = line;
+token new_token(token_kind k, const char *literal, int line) {
+	token t;
+	t.kind = k;
+	t.literal = literal;
+	t.line = line;
 	return t;
+}
+
+void *append(void *ptr, int *len, int *cap, size_t size) {
+	if (*cap == 0 || *len + 1 > *cap) {
+		if (*cap == 0) {
+			*cap = 4;
+		} else {
+			*cap *= 2;
+		}
+		ptr = realloc(ptr, size * (*cap));
+	}
+	return ptr;
 }
 
 lexer_result *lexer(const char *buf, int fsize) {
 	lexer_result *l = (lexer_result *) malloc(sizeof(lexer_result));
-	l->count = 0;
 	for (int i = 0, e = 1; i < fsize; i ++) {
 		char c = buf[i];
-space:		while (is_space(c, i, fsize)) {
-			if (c == '\n') e ++;
+space:		while (is_space(c)) {
+			if (c == '\n') {
+				printf("LINE %d\n", e + 1);
+				e ++;
+			}
 			c = buf[++ i];
 		}
+		/*
 block:		if (peek_to(c, '*', i, fsize, buf)) {
 			i += 2;
 			while (i < fsize) {
@@ -65,6 +77,7 @@ line:		if  (peek_to(c, '/', i, fsize, buf)) {
 		if (is_space(c, i, fsize))          goto space;
 		if (peek_to(c, '*', i, fsize, buf)) goto block;
 		if (peek_to(c, '/', i, fsize, buf)) goto line;
+		*/
 		if (is_digit(c)) {
 			int p = 0;
 			while (is_digit(c)) {
@@ -76,9 +89,10 @@ line:		if  (peek_to(c, '/', i, fsize, buf)) {
 				literal[x] = buf[i - p];
 				p --;
 			}
-			l->tokens[l->count++] = new_token(NUMBER, literal, e);
+			l->tokens = append(l->tokens, &l->len, &l->cap, sizeof(token));
+			l->tokens[l->len++] = new_token(NUMBER, literal, e);
 		}
-		if (is_identifier(c)) {
+		if (is_ident(c)) {
 			printf("IDENTIFIER: %c LINE: %d\n", c, e);
 		}
 	}
@@ -104,11 +118,11 @@ int main(int argc, char **argv) {
 	
 	fread(buf, fsize, sizeof(char), fp);
 	buf[fsize] = '\0';
-	
-	lexer_result *l = lexer(buf, fsize);
-	for (int i = 0; i < l->count; i ++) {
-		token *tok = l->tokens[i];
-		printf("%d %s %d\n", tok->kind, tok->literal, tok->line);
+
+	lexer_result *lex = lexer(buf, fsize);
+	for (int i = 0; i < lex->len; i ++) {
+		token t = lex->tokens[i];
+		printf("TOKEN: %d %s %d\n", t.kind, t.literal, t.line);
 	}
 	
 	fclose(fp);
